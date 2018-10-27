c7cc55eee906c8c601c02e5a9b1ff10b4726e887
Sai <44411581+maw3oud@users.noreply.github.com>
2018-10-27 15:03:03 -0400

    Add files via upload

---
+++ assets/title_animation/animation.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+$(function(){
+	$('.repeat').mouseover(function(){
+    	var classes =  $(this).parent().attr('class');
+        $(this).parent().attr('class', 'animate');
+        var indicator = $(this);
+        setTimeout(function(){ 
+        	$(indicator).parent().addClass(classes);
+        }, 20);
+    });
+});
